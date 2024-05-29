9df022f5a5b6d0422107bfbcb63210191027c02a
jiiiiiw <98935125+jiiiiiw@users.noreply.github.com>
2024-05-29 16:32:36 +0900

    JPA를 통한 엔티티 설계, 매핑 (#1)

---
+++ src/main/java/umcstudybook/spring/domain/Member.java
@@ -0,0 +1,66 @@
+package umcstudybook.spring.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import umcstudybook.spring.domain.common.BaseEntity;
+import umcstudybook.spring.domain.enums.Gender;
+import umcstudybook.spring.domain.enums.MemberStatus;
+import umcstudybook.spring.domain.enums.SocialType;
+import umcstudybook.spring.domain.mapping.MemberAgree;
+import umcstudybook.spring.domain.mapping.MemberMission;
+import umcstudybook.spring.domain.mapping.MemberPrefer;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 20)
+    private String name;
+
+    @Column(nullable = false, length = 40)
+    private String address;
+
+    @Column(nullable = false, length = 40)
+    private String specAddress;
+
+    @Enumerated(EnumType.STRING)
+    @Column(columnDefinition = "VARCHAR(10)")
+    private Gender gender;
+
+    @Enumerated(EnumType.STRING)
+    private SocialType socialType;
+
+    @Enumerated(EnumType.STRING)
+    @Column(columnDefinition = "VARCHAR(15) DEFAULT 'ACTIVE'")
+    private MemberStatus status;
+
+    private LocalDate inactiveDate;
+
+    @Column(nullable = false, length = 50)
+    private String email;
+
+    private Integer point;
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
+    private List<MemberAgree> memberAgreeList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
+    private List<MemberPrefer> memberPreferList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
+    private List<Review> reviewList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
+    private List<MemberMission> memberMissionList = new ArrayList<>();
+}
---
+++ src/main/java/umcstudybook/spring/domain/mapping/MemberPrefer.java
@@ -0,0 +1,27 @@
+package umcstudybook.spring.domain.mapping;
+
+import jakarta.persistence.*;
+import lombok.*;
+import umcstudybook.spring.domain.FoodCategory;
+import umcstudybook.spring.domain.Member;
+import umcstudybook.spring.domain.common.BaseEntity;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class MemberPrefer extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private FoodCategory foodCategory;
+}
